d0ee4d261320279d9016197685fa8d2975ab0d98
codej625 <codej625@gmail.com>
2024-01-31 15:44:53 +0900

    20240131 update

---
+++ react_start/hook/src/App.js
@@ -0,0 +1,64 @@
+import "./App.css";
+import React, { useState } from "react";
+
+function App() {
+
+  const [count, setCount] = useState(() => {
+    return 0;
+  });
+
+  function plus() {
+    setCount((prev) => {
+      return prev + 1;
+    });
+  }
+
+  function minus() {
+    setCount((prev) => {
+      if (prev === 0) {
+        return prev;
+      }
+      return prev - 1;
+    });
+  }
+
+  function reset() {
+    setCount((prev) => {
+      return prev = 0;
+    });
+  }
+
+  return (
+    <>
+      <div className="container">
+        <button
+          className="btn-nomal"
+          type="button" 
+          onClick={plus}
+        >
+          더하기
+        </button>
+
+        <div className="count-display">{count}</div>
+
+        <button
+          className="btn-nomal"
+          type="button" 
+          onClick={minus}
+        >
+          빼기
+        </button>
+        <br />
+        <button
+          className="btn-reset"
+          type="button" 
+          onClick={reset}
+        >
+          리셋
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
