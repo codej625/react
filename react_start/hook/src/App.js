c7b1fd66d23104cbe9df3c28a89f686a9ec38139
codej625 <codej625@gmail.com>
2024-02-01 23:46:29 +0900

    20240201 input test

--- react_start/hook/src/App.js
+++ react_start/hook/src/App.js
@@ -1,63 +1,81 @@
 import "./App.css";
+// import Count from "./Count";
 import React, { useState } from "react";
 
 function App() {
-
-  const [count, setCount] = useState(() => {
-    return 0;
+  const [insert, setInsert] = useState(()=> {
+    return {
+      user: '',
+      age: '',
+    };
   });
+  
+  const [inputs, setInput] = useState(()=> {
+    return {
+      user: '',
+      age: '',
+    };
+  });
+  const {user, age} = inputs;
 
-  function plus() {
-    setCount((prev) => {
-      return prev + 1;
+  function inputChange(e) {
+    const {name, value} = e.target;
+
+    setInput({
+      ...inputs,
+      [name]: value
     });
   }
 
-  function minus() {
-    setCount((prev) => {
-      if (prev === 0) {
-        return prev;
-      }
-      return prev - 1;
-    });
-  }
-
-  function reset() {
-    setCount((prev) => {
-      return prev = 0;
+  function inputInsert() {
+    setInsert({
+      ...insert,
+      ...inputs,
+    })
+    setInput({
+      ...inputs, 
+        user: '',
+        age: ''
     });
   }
 
   return (
-    <>
-      <div className="container">
-        <button
-          className="btn-nomal"
-          type="button" 
-          onClick={plus}
+    <div className="container">
+      <div>
+        <label htmlFor="name" className="label-style">이름 </label>
+        <input 
+          id="name" 
+          name="user" 
+          type="text" 
+          onChange={inputChange} 
+          value={user}
+        />
+      </div>
+      <div>
+        <label htmlFor="age" className="label-style">나이 </label>
+        <input 
+          id="age" 
+          name="age" 
+          type="text" 
+          onChange={inputChange} 
+          value={age}
+        />
+      </div>
+      <div className="check-btn">
+        <span className="contents">
+          이름: {insert.user}
+        </span>
+        <span className="contents">
+          나이: {insert.age}
+        </span>
+        <button 
+          onClick={inputInsert} 
+          type="button"
         >
-          더하기
-        </button>
-
-        <div className="count-display">{count}</div>
-
-        <button
-          className="btn-nomal"
-          type="button" 
-          onClick={minus}
-        >
-          빼기
-        </button>
-        <br />
-        <button
-          className="btn-reset"
-          type="button" 
-          onClick={reset}
-        >
-          리셋
+          확인
         </button>
       </div>
-    </>
+    </div>
   );
 }
 
