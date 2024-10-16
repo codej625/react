21689390b64bbb67255556c8902f49d9e228fa1b
codej625 <codej625@gmail.com>
2024-10-17 00:27:00 +0900

    241017 Update store by codej625

--- react_projects/vite-project/src/components/pages/StoreTest.jsx
+++ react_projects/vite-project/src/components/pages/StoreTest.jsx
@@ -3,13 +3,8 @@ import {useState} from 'react';
 
 export default function StoreTest() {
 
-  const [input, setInput] = useState({input: 0});
-  const { 
-    bears, 
-    increasePopulation,
-    removeAllBears,
-    updateBears
-  } = useStore();
+  const [ input, setInput ] = useState({input: 0});
+  const { bears, increasePopulation, removeAllBears, updateBears } = useStore();
 
   const setInputValue = (e) => {
     const value = e.target.value;
@@ -18,12 +13,35 @@ export default function StoreTest() {
     }));
   };
 
+  const removeInputValue = () => {
+    setInput({input: 0});
+    removeAllBears(input.input);
+  };
+
   return (
     <>
-      {/* <input name="input" value={input.input} onChange={setInputValue} type="number" /> */}
       <h1>{bears} around here...</h1>
-      <button onClick={increasePopulation}>one up</button>
-      <button onClick={removeAllBears}>remove</button>
+      <input 
+        name="input" 
+        value={input.input} 
+        onChange={setInputValue} 
+        type="number" 
+        />
+      <button 
+        onClick={increasePopulation}
+      >
+        one up
+      </button>
+      <button 
+        onClick={removeInputValue}
+      >
+        remove
+      </button>
+      <button 
+        onClick={() => updateBears(input.input)}
+      >
+        update
+      </button>
     </>
   )
 } 
--- react_projects/vite-project/src/store/store.js
+++ react_projects/vite-project/src/store/store.js
@@ -4,7 +4,7 @@ const useStore = create((set) => ({
   bears: 0,
   increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
   removeAllBears: () => set({ bears: 0 }),
-  updateBears: (newBears) => set({ bears: newBears }),
+  updateBears: (newBears) => set({ bears: Number(newBears) }),
 }))
 
 export default useStore
