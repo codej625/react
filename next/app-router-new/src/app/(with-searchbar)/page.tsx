15b23d45b270e9fef78febc814bdd6e0c1c13f64
codej625 <codej625@gmail.com>
2025-01-19 04:29:51 +0900

    250119 Updated next project. by codej625

--- next/app-router-new/src/app/(with-searchbar)/page.tsx
+++ next/app-router-new/src/app/(with-searchbar)/page.tsx
@@ -1,21 +1,56 @@
 import BookItem from "@/components/book-item";
 import style from "./page.module.css";
 import books from "@/mock/books.json";
+import { BookData } from "@/types";
 
-export default function Home() {
+const AllBooks = async () => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`
+  );
+
+  if (!response.ok) {
+    return <div>서버에서 도서 목록을 불러오는 중 오류가 발생했습니다.</div>;
+  }
+  const allBooks: BookData[] = await response.json();
+
+  return (
+    <>
+      {allBooks.map((book, index: number) => (
+        <BookItem key={`${book.id}-${index}`} {...book} />
+      ))}
+    </>
+  );
+};
+
+const RandomBooks = async () => {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`
+  );
+
+  if (!response.ok) {
+    return <div>서버에서 도서 목록을 불러오는 중 오류가 발생했습니다.</div>;
+  }
+  const randomBooks: BookData[] = await response.json();
+
+  return (
+    <>
+      {randomBooks.map((book, index: number) => (
+        <BookItem key={`${book.id}-${index}`} {...book} />
+      ))}
+    </>
+  );
+};
+
+export default async function Home() {
   return (
     <div className={style.container}>
+      <h3>지금 추천하는 도서</h3>
       <section>
-        <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        <RandomBooks />
       </section>
+      <h3>등록된 모든 도서</h3>
       <section>
-        <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        <AllBooks />
       </section>
     </div>
   );
--- next/app-router-new/src/app/(with-searchbar)/search/page.tsx
+++ next/app-router-new/src/app/(with-searchbar)/search/page.tsx
@@ -1,13 +1,24 @@
-import books from "@/mock/books.json";
+// import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
+import { BookData } from "@/types";
 
-export default function Page({
+export default async function Page({
   searchParams,
 }: {
-  searchParams: Promise<{
-    q?: string;
-  }>;
+  searchParams: Promise<{ [q: string | number]: string }>;
 }) {
+  const params = await searchParams;
+  const q = params.q;
+
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`
+  );
+
+  if (!response.ok) {
+    return <div>오류가 발생했습니다.</div>;
+  }
+  const books: BookData[] = await response.json();
+
   return (
     <div>
       {books.map((book) => (
